aa290dda4d56489e02052bee78653956e11f1c87
Pedro Juan Ferreira Saraiva <pedrojuanferreirasaraiva@192.168.100.14>
2025-02-20 15:57:56 -0300

    Primeiro commit - Configuração inicial do projeto

---
+++ toDoList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  toDoList
+//
+//  Created by Pedro Juan Ferreira Saraiva on 20/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ toDoList/toDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoListApp.swift
+//  toDoList
+//
+//  Created by Pedro Juan Ferreira Saraiva on 20/02/25.
+//
+
+import SwiftUI
+
+@main
+struct toDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
